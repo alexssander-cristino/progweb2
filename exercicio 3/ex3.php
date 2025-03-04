90094053e7839f1c170d56e65b1ff0f40515bb3f
alexssander-cristino <alexssander.cristino@unidavi.edu.br>
2025-03-03 22:28:47 -0300

    exercico 3

--- exercicio 3/ex3.php
+++ exercicio 3/ex3.php
@@ -3,12 +3,23 @@
 // Carregar o autoload do Composer
 require_once 'vendor/autoload.php';
 
-// Criar um gerador de dados falsos
+// Criar o gerador de dados falsos
 $faker = \Faker\Factory::create();
 
 // Gerar dados falsos
-echo "CPF: " . $faker->cpf() . "\n";
-echo "Data de Nascimento: " . $faker->date('d/m/Y') . "\n";
-echo "Telefone: " . $faker->phoneNumber() . "\n";
-echo "Produto: " . $faker->word() . "\n";
-echo "Preço: R$ " . $faker->randomFloat(2, 1, 1000) . "\n";
+$nome = $faker->name;
+$email = $faker->email;
+$cpf = $faker->numerify('###.###.###-##');
+$endereco = $faker->address;
+$telefone = $faker->phoneNumber;
+$data_nascimento = $faker->date('d/m/Y');
+
+// Exibir os dados gerados
+echo "Nome: " . $nome . "\n";
+echo "Email: " . $email . "\n";
+echo "CPF: " . $cpf . "\n";
+echo "Endereço: " . $endereco . "\n";
+echo "Telefone: " . $telefone . "\n";
+echo "Data de Nascimento: " . $data_nascimento . "\n";
+
+?>
